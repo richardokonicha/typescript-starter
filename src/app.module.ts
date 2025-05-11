b5f6c0ea0b4b22f6009495e5bb86a8242e268b2b
Rich <RO@rdeck.local>
2025-05-11 19:56:32 +0100

    update routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,61 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import path from "node:path";
+
+import { HttpException, Module } from "@nestjs/common";
+import { APP_INTERCEPTOR, APP_PIPE } from "@nestjs/core";
+// import { ServeStaticModule } from "@nestjs/serve-static";
+import { RavenInterceptor, RavenModule } from "nest-raven";
+import { ZodValidationPipe } from "nestjs-zod";
+
+// import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "./config/config.module";
+// import { ContributorsModule } from "./contributors/contributors.module";
+import { DatabaseModule } from "./database/database.module";
+// import { FeatureModule } from "./feature/feature.module";
+// import { HealthModule } from "./health/health.module";
+import { MailModule } from "./mail/mail.module";
+// import { PrinterModule } from "./printer/printer.module";
+// import { ResumeModule } from "./resume/resume.module";
+// import { StorageModule } from "./storage/storage.module";
+// import { TranslationModule } from "./translation/translation.module";
+// import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Core Modules
+    ConfigModule,
+    DatabaseModule,
+    MailModule,
+    RavenModule,
+    // HealthModule,
+
+    // Feature Modules
+    // AuthModule.register(),
+    // UserModule,
+    // ResumeModule,
+    // StorageModule,
+    // PrinterModule,
+    // FeatureModule,
+    // TranslationModule,
+    // ContributorsModule,
+
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useValue: new RavenInterceptor({
+        filters: [
+          // Filter all HttpException with status code <= 500
+          {
+            type: HttpException,
+            filter: (exception: HttpException) => exception.getStatus() < 500,
+          },
+        ],
+      }),
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,66 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+// import cookieParser from "cookie-parser";
+import * as cookieParser from 'cookie-parser';
+
+import helmet from "helmet";
+import { patchNestJsSwagger } from "nestjs-zod";
+
+import { AppModule } from "./app.module";
+import { Config } from "./config/schema";
+
+patchNestJsSwagger();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: process.env.NODE_ENV === "development" ? ["debug"] : ["error", "warn", "log"],
+  });
+  const configService = app.get(ConfigService<Config>);
+
+  // Cookie Parser
+  app.use(cookieParser());
+
+  // CORS
+  app.enableCors({
+    credentials: true,
+    origin: process.env.NODE_ENV === "production",
+  });
+
+  // Helmet - enabled only in production
+  if (process.env.NODE_ENV === "production") {
+    app.use(helmet({ contentSecurityPolicy: false }));
+  }
+
+  // Global Prefix
+  const globalPrefix = "api";
+  app.setGlobalPrefix(globalPrefix);
+
+  // Enable Shutdown Hooks
+  app.enableShutdownHooks();
+
+  // Swagger (OpenAPI Docs)
+  // This can be accessed by visiting {SERVER_URL}/api/docs
+  const config = new DocumentBuilder()
+    .setTitle("Grzy")
+    .setDescription(
+      "Grzy is a simple, resume builder designed to make creating and sharing resumes effortless.",
+    )
+    .addCookieAuth("Authentication", { type: "http", in: "cookie", scheme: "Bearer" })
+    .setVersion("4.0.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
+  // Port
+  const port = configService.get<number>("PORT") ?? 3000;
+
+  await app.listen(port);
+
+  Logger.log(`🚀 Server is up and running on port ${port}`, "Bootstrap");
 }
-bootstrap();
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+void bootstrap();
